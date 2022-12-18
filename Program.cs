7d3b88057705393cb1b7f3f44ca4362bb05e552e
Matt-Jam <mattyjamson@gmail.com>
2022-12-18 13:23:27 +0000

    Multi-shape test and removed debugging

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ class Program
         // Makes window with width and height and background colour
         Window testWindow = new Window(500,500,Colour.White);
         // Array of points for shape
-        (float, float)[] points = {(20,10), (100,10),(50,50)};
+        (float, float)[] points1 = {(20,10), (100,10),(50,50)};
+        (float, float)[] points2 = {(20,30), (100,10),(50,50)};
         // Makes shapes
-        NGon testShape = new NGon(points, Colour.Blue);
+        NGon testShape = new NGon(points1, Colour.Blue);
+        NGon testShape2 = new NGon(points2,Colour.Red);
         // Add shape to screen
         testShape.Add(testWindow);
+        testShape2.Add(testWindow);
         // Show screen
         testWindow.Show();
-        Console.WriteLine("Pog");
     }
 }
